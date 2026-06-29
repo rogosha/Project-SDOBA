76f9b24e344b3ffed406d525cf9e3854e3986402
Rogosha <alexandr.kachalov.prof@gmail.com>
2026-06-29 15:56:35 +0300

    Initial MAUI project

---
+++ Project SDOBA/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Project_SDOBA;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Project SDOBA/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Project_SDOBA;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
